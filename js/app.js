98c6e277316522481e2f64b31ab9ee9f0e5274bd
KhanVl <vovahan2002@gmail.com>
2024-11-14 16:53:12 +0900

    new-commit

--- js/app.js
+++ js/app.js
@@ -68,24 +68,26 @@ document.querySelector('.button.call__close').addEventListener('click', function
     
 })
 
+// Слайдер // 
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: true,
+    speed: 500,
+    slidesPerView: 1.2,
+    spaceBetween: 10,
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
+
+    // autoplay:{
+
+    //     // delay: 3000,
+    // },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
   
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-});
+  });
+
